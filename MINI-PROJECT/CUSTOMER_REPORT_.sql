df33cd8839a03040981476970be9af5380fa6900
Ayush Shankar Prasad <prasadshankarayush002@gmail.com>
2026-03-21 10:57:15 +0530

    Add files via upload

---
+++ MINI-PROJECT/CUSTOMER_REPORT_.sql
@@ -0,0 +1,54 @@
+USE SalesDB;
+--Find the Total Sales per Customer
+WITH CTE_Total AS
+(
+Select	CustomerId,
+		SUM(Sales) As Total_Sales
+from Sales.Orders
+Group By CustomerID
+)
+--Find the Last ORder Date Per Customer
+,CTE_Lastorder AS
+(
+select CustomerID,
+	MAX(OrderDate) as Last_order
+from Sales.Orders
+GROUP BY CustomerID
+)
+--Rank Customer based on totals sales
+, CTE_Customer_Rank AS
+(
+	select 
+	CustomerID,
+	Total_Sales,
+	RANK() OVER( ORDER BY Total_Sales DESC) AS CustomerRank
+	from CTE_Total
+)
+--Segment Customer Based on their Total Sales
+, CTE_Customer_Segment AS(
+	select CustomerID,
+CASE	
+	WHEN Total_Sales>100 then 'HIGH'
+	WHEN Total_Sales>50 then 'MEDIUM'
+	ELSE 'LOW'
+	END Customer_Segment
+	from CTE_Total
+)
+--MAIN Query
+Select 
+	c.CustomerID,
+	c.FirstName,
+	c.LastName,
+	cts.Total_Sales,
+	clo.Last_order,
+	ccr.CustomerRank,
+	ccs.Customer_Segment
+from Sales.Customers c
+LEFT JOIN CTE_Total cts
+on cts.CustomerID=c.CustomerID
+LEFT JOIN CTE_Lastorder clo
+on clo.CustomerID=c.CustomerID
+LEFT JOIN CTE_Customer_Rank ccr
+on ccr.CustomerID=c.CustomerID
+LEFT JOIN CTE_Customer_Segment ccs
+on ccs.CustomerID=c.CustomerID
